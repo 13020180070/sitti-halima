364fd69e93f39f152d8832fe76c9c62e2d714f50
Siti Halima <62000373+13020180070@users.noreply.github.com>
2020-03-16 22:17:14 +0800

    Add files via upload

---
+++ halima pbo/limun.java
@@ -0,0 +1,18 @@
+/*NamaFile : halima
+/*Hari/tgl/waktu: senin,16 mar 2020 21.37
+
+
+public class limun{
+	public static void main(String[] args){
+	String nim ="13020180070";
+	String nama ="Sitti Halima";
+	String jurusan ="Teknik Informatika";
+	String fakultas ="ilmu komputer";
+	System.out.println("nim saya: "+ nim);
+	System.out.println("nama saya: "+ nama);
+	System.out.println("jurusan: "+ jurusan);
+	System.out.println("fakultas: "+ fakultas);
+
+}
+}
+	
